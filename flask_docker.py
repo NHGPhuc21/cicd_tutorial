ac504630fb66dbd852d9b7ae77f91d30e8f9ce17
Nguyễn Hồ Gia Phúc <137470912+NHGPhuc21@users.noreply.github.com>
2025-07-08 18:19:47 +0700

    Update flask_docker.py

--- flask_docker.py
+++ flask_docker.py
@@ -7,4 +7,4 @@ def hello_world():
 	return 'Hello FOSS, my name Nguyen Ho Gia Phuc, Hello CI/CD using Jenkins'
 
 if __name__ == '__main__':
-	app.run(debug=True, host='0.0.0.0, port=5000')
+	app.run(debug=True, host='0.0.0.0')
